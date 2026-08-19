0ff8cb43f32dadca7849ef8900ad22035a4f0857
manuelliberal-oss <manuel.liberal@naturalth.it>
2026-08-19 18:03:59 -0300

    fis grilla ultimas noticias

--- components/news/LatestNewsSectionWithFetch.tsx
+++ components/news/LatestNewsSectionWithFetch.tsx
@@ -3,9 +3,10 @@ import { getNewsListItems } from '@/lib/contentful/queries';
 import { LatestNewsSection } from './LatestNewsSection';
 
 const LATEST_NEWS_COUNT = 4;
+const LATEST_NEWS_FETCH_BUFFER = LATEST_NEWS_COUNT * 2;
 
 export async function LatestNewsSectionWithFetch() {
-  const items = await getNewsListItems(LATEST_NEWS_COUNT);
+  const items = await getNewsListItems(LATEST_NEWS_FETCH_BUFFER);
 
-  return <LatestNewsSection items={items} />;
+  return <LatestNewsSection items={items.slice(0, LATEST_NEWS_COUNT)} />;
 }
